8f6e6b8c3bb6600306a2a62c2643a0e4e4bcc30e
Sefa <sbaah12@gmail.com>
2021-01-24 23:21:33 -0500

    create sign-up and sign-in, custome-button, form-input components and sign-in and sign-up pages. With all errors fixed

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FormInput } from '../form-input/form-input.component';
+import { CustomButton } from '../custom-button/custom-button.component';
+import './sign-in.styles.scss';
+
+export class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  // method for submission fo the form
+  handleSubmit = (event) => {
+    // to prevent the default behaviour of the form and give me control
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    // dynamically set the the date due their target.
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2> I already have an account </h2>
+        <span> Sign in with you email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label='Email'
+            required
+          />
+
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='Password'
+            required
+          />
+
+          <CustomButton type='submit'> Sign in </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
