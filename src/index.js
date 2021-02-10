af76fc00452e7b049fc0be1ace07c6b9a2b70093
Sefa <sbaah12@gmail.com>
2021-02-10 17:57:40 -0500

    introducee local storage with react-persist and incoprated cart for use

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 // redux
 // npm install redux redux-logger redux-react
@@ -16,7 +16,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,8 +6,19 @@ Will be the actual code that combines all others states together
 import { combineReducers } from 'redux';
 import useReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import { persistReducer } from 'redux-persist';
+// local storage
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: useReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
