ee56dd4fa7f8f0b7ccfcdb1c9ebc1955d0fb766a
Sefa <sbaah12@gmail.com>
2021-07-09 22:21:33 -0400

    setting logger to  display only on test not on dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,14 @@ import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 /**
  * Is a combination of the root reducer and the middleware
- * for the loggin the output of the what going on ...
+ * for the logging the output of the what going on ...
  */
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
